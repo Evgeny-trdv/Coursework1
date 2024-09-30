b672656e5103a2ba470fdf94a33291229e78312e
evgen <evgeny15q@gmail.com>
2024-09-30 21:17:18 +0700

    Start

---
+++ src/FullName.java
@@ -0,0 +1,11 @@
+public class FullName {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+
+    FullName(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Empoyee empoyee1 = new Empoyee(new FullName("Ivan", "Ivanovich", "ivanov"), 1, 50000);
+    }
+}
